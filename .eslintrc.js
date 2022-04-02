584084361e3788d8d0218947b1bcbf20a704efa9
qiaozhi <1950598869@qq.com>
2022-04-02 23:29:10 +0800

    feat: 更改了eslint配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -235,7 +235,7 @@ module.exports = {
       }
     ],
     'space-before-blocks': [2, 'always'],
-    'space-before-function-paren': [1, 'never'],
+    'space-before-function-paren': [0, 'never'],
     'space-in-parens': [2, 'never'],
     'space-infix-ops': 2,
     'space-unary-ops': [
